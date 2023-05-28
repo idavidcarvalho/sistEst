e4e3a179e43f614d5a7ece3deb0f8045511e751b
David <davidsoares585@gmail.com>
2023-05-28 14:12:11 -0300

    inclusão dos modelos 'Carros' e 'Estacionamentos'

--- envSistEst/SisEst/usuarios/admin.py
+++ envSistEst/SisEst/usuarios/admin.py
@@ -1,9 +1,21 @@
 from django.contrib import admin
-from usuarios.models import Usuarios
+from usuarios.models import *
 
 # Register your models here.
 
 class UsuarioAdmin(admin.ModelAdmin):
     ...
 
+class CarrosAdmin(admin.ModelAdmin):
+    ...
+
+class EstacionamentosAdmin(admin.ModelAdmin):
+    ...
+
 admin.site.register(Usuarios, UsuarioAdmin)
+
+admin.site.register(Carros, CarrosAdmin)
+
+admin.site.register(Estacionamentos, EstacionamentosAdmin)
+
+
--- envSistEst/SisEst/usuarios/models.py
+++ envSistEst/SisEst/usuarios/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Usuarios(models.Model):
-   idUsuario = models.IntegerField(primary_key=True)
+   idUsuario = models.BigAutoField(primary_key=True)
    nome = models.CharField(max_length=300, null=False)
    cpf = models.CharField(max_length=11, null=False)
    num_CNH = models.CharField(max_length=15, null=True)
@@ -10,6 +10,43 @@ class Usuarios(models.Model):
                                    help_text="Formato <em>YYYY-MM-DD</em>")
    telefone = models.CharField(max_length=15, null=False)
    email = models.CharField(max_length=300,null=True)
+   login =  models.CharField(max_length=300, null=True)
+
+   class Meta:
+      verbose_name_plural ="Usuários"
 
    def __str__(self):
       return self.cpf + " - " + self.nome 
+
+class Carros(models.Model):
+   idCarro = models.BigAutoField(primary_key=True)
+   placa = models.CharField(max_length=10, null=False)
+   marca_modelo = models.CharField(max_length=300, null=False)
+   cor = models.CharField(max_length=50, null=False)
+   fk_idUsuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+
+   class Meta:
+      verbose_name_plural ="Carros"
+
+   def __str__(self):
+      return self.placa
+
+
+class Estacionamentos(models.Model):
+   idEstacionamento = models.BigAutoField(primary_key=True)
+   data_entrada = models.DateField(auto_now_add=True, null=False,
+                                   help_text="Formato <em>YYYY-MM-DD</em>")
+   hora_entrada = models.TimeField(auto_now_add=True, null=False,
+                                   help_text="Formato <em>HH-MM-SS</em>")
+   data_saida = models.DateField(null=True, blank=True,
+                                   help_text="Formato <em>YYYY-MM-DD</em>")
+   hora_saida = models.TimeField(null=True, blank=True,
+                                   help_text="Formato <em>HH-MM-SS</em>")
+   fk_IdCarro = models.ForeignKey(Carros, on_delete=models.CASCADE, verbose_name="Placa do Carro")
+
+   class Meta:
+      verbose_name_plural ="Estacionamentos"
+
+   def __str__(self) -> str:
+      return self.data_entrada.strftime("%Y/%m/%d") + " - " + self.hora_entrada.strftime("%H:%M:%S")
+
